f6e18f491e94c6bd114c209c69b6590b15688480
saraang <saraangsk@gmail.com>
2019-07-12 11:38:59 +0530

    login page added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent ],
+  declarations: [ AppComponent, LoginComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { 
-  name: string;
+  username: string;
 }
