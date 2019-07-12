895f36fe551fcecfa9244e048740ecb9e5b6a46d
saraang <saraangsk@gmail.com>
2019-07-12 13:58:40 +0530

    input fields added successfully

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -7,9 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  username = 'Sarang';
+  password = '';
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  loginEvent(){
+    console.log(this.username);
   }
 
 }
