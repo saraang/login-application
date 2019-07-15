89199b0eeae78f389e15c666381cdd45b483964a
saraang <saraangsk@gmail.com>
2019-07-15 18:11:47 +0530

    added routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, LoginComponent ],
+  declarations: [ AppComponent, LoginComponent, WelcomeComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { 
