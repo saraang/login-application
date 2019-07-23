13116c69047bef127ccfd9a69d7be8dc99fe41b7
saraang <saraangsk@gmail.com>
2019-07-23 15:53:17 +0530

    Logout functionality implemented successfully

--- src/app/service/route-guard.service.ts
+++ src/app/service/route-guard.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { Router, CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 import { HardcodedAuthenticationService } from './../service/hardcoded-authentication.service';
 
 @Injectable({
@@ -7,11 +7,12 @@ import { HardcodedAuthenticationService } from './../service/hardcoded-authentic
 })
 export class RouteGuardService implements CanActivate{
 
-  constructor(private hardcodedAuthenticationService: HardcodedAuthenticationService) { }
+  constructor(private hardcodedAuthenticationService: HardcodedAuthenticationService, private router: Router) { }
 
   canActivate(route: ActivatedRouteSnapshot,state: RouterStateSnapshot){
     if(this.hardcodedAuthenticationService.isUserLoggedIn())
       return true;
+    this.router.navigate(['login']);
     return false;
   }
 
